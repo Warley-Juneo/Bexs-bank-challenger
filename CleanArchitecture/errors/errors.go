9461f3406dab3499d41f830b5bb9ab6017f68ff4
warley-juneo <warleyjuneo111@gmail.com>
2023-08-19 23:37:54 -0300

    feat: adding currency validation, improving msg errors system

---
+++ CleanArchitecture/errors/errors.go
@@ -0,0 +1,7 @@
+package errors
+
+const (
+	ErrPartnerAlreadyExists = "a partner with that document already exists"
+	ErrCurrencyNotAllowed   = "currency not allowed"
+	ErrInternalServerError  = "internal server error"
+)
--- CleanArchitecture/services/partnerservices/partnerservice.go
+++ CleanArchitecture/services/partnerservices/partnerservice.go
@@ -10,6 +10,7 @@ import (
 	"github.com/wjuneo/bexs/entity"
 	"github.com/wjuneo/bexs/logs"
 	"github.com/wjuneo/bexs/repository/partnerrepository"
+	"github.com/wjuneo/bexs/errors"
 )
 
 type PartnerService interface {
@@ -27,11 +28,27 @@ func NewPartnerService(partnerRepository partnerrepository.PartnerRepository) Pa
 	}
 }
 
+func ValidatedCurrency(currency string) bool {
+	allowedCurrencies := []string{"GBP", "EUR", "USD"}
+
+	for _, allowedCurrency := range allowedCurrencies {
+		if allowedCurrency == currency {
+			return true
+		}
+	}
+	return false
+}
+
 func (ps *partnerService) ValidatePartners(partnerData partnerdto.PartnerData) error {
 	partner, _ := ps.partnerRepository.FindPartnerByDocument(context.Background(), partnerData.Document)
 	if partner != nil {
-		logs.LogToFile("logs/error.log", "a partner with that document already exists")
-		return fmt.Errorf("a partner with that document already exists")
+		logs.LogToFile("logs/error.log", errors.ErrPartnerAlreadyExists)
+		return fmt.Errorf(errors.ErrPartnerAlreadyExists)
+	}
+
+	if !ValidatedCurrency(partnerData.Currency) {
+		logs.LogToFile("logs/error.log", errors.ErrCurrencyNotAllowed)
+		return fmt.Errorf(errors.ErrCurrencyNotAllowed)
 	}
 	return nil
 }
@@ -45,15 +62,19 @@ func (ps *partnerService) HandlerRequest(w http.ResponseWriter, r *http.Request)
 
 	entity, err := ps.SavePartners(partnerData)
 	if err != nil {
-		if err.Error() == "a partner with that document already exists" {
+		if err.Error() == errors.ErrPartnerAlreadyExists {
 			w.WriteHeader(http.StatusConflict)
-			w.Write([]byte("a partner with that document already exists"))
+			w.Write([]byte(errors.ErrPartnerAlreadyExists))
+			return
+		} else if err.Error() == errors.ErrCurrencyNotAllowed {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(errors.ErrCurrencyNotAllowed))
 			return
 		} else {
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal server error"))
+			w.Write([]byte(errors.ErrInternalServerError))
 			return
-		}
+		} 
 	}
 
 	w.WriteHeader(http.StatusCreated)
@@ -77,8 +98,8 @@ func (ps *partnerService) SavePartners(partnerData partnerdto.PartnerData) (*ent
 	ctx := context.Background()
 	newEntity, err := ps.partnerRepository.SavePartners(ctx, entity)
 	if err != nil {
-		logs.LogToFile("logs/error.log", "failed to save partner in database")
-		return nil, fmt.Errorf("failed to save partner")
+		logs.LogToFile("logs/error.log", errors.ErrInternalServerError)
+		return nil, fmt.Errorf(errors.ErrInternalServerError)
 	}
 
 	return newEntity, nil
